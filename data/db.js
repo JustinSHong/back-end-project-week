4493960ca44a34c41935e914a3f1cb232a4c32cf
JustinSHong <jhong2009@gmail.com>
2018-10-08 15:25:36 -0700

    used env vars inside uri

--- data/db.js
+++ data/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
-//<dbuser>:<dbpassword>@ds139970.mlab.com:39970/lambdanotes-justinh
+const user = process.env.DB_user;
+const password = process.env.DB_password;
+const host = process.env.DB_host;
+const dbName = process.env.DB_name;
+
 mongodb: module.exports = {
-	connectTo: function() {
-		return mongoose.connect(
-			encodeURI(
-				`mongodb://justin:password123@ds139970.mlab.com:39970/lambdanotes-justinh`
-			)
-		);
-	}
+    connectTo: function() {
+        return mongoose.connect(
+            encodeURI(
+                `mongodb://${user}:${password}@ds139970.mlab.com:${host}/${dbName}`
+            )
+        );
+    }
 };
