64e4fde2fc1746cf0351c6b2299020e8e3a5bce7
JustinSHong <jhong2009@gmail.com>
2018-10-08 16:54:48 -0700

    set env var for mongodb uri

--- data/db.js
+++ data/db.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const mongoURL = process.env.MONGODB_URI;
 
 mongodb: module.exports = {
     connectTo: function() {
-        return mongoose.connect(encodeURI(process.env.MONGODB_URI));
+        return mongoose.connect(
+            encodeURI(mongoURL),
+            { native_parser: true }
+        );
     }
 };
