78eb718d6a7808fa37a6c82c099193feac6033df
JustinSHong <jhong2009@gmail.com>
2018-10-08 15:42:19 -0700

    changed env var

--- data/db.js
+++ data/db.js
@@ -1,15 +1,7 @@
 const mongoose = require("mongoose");
-const user = process.env.DB_user;
-const password = process.env.DB_password;
-const host = process.env.DB_host;
-const dbName = process.env.DB_name;
 
 mongodb: module.exports = {
     connectTo: function() {
-        return mongoose.connect(
-            encodeURI(
-                `mongodb://${user}:${password}@ds139970.mlab.com:${host}/${dbName}`
-            )
-        );
+        return mongoose.connect(encodeURI(process.env.MONGODB_URI));
     }
 };
