92d613bfd5c20c5cf5bf58d40fa2c1329d25b29a
JustinSHong <jhong2009@gmail.com>
2018-10-08 20:53:35 -0700

    added isComplete, id, archive fields to Note Schema

--- notes/Note.js
+++ notes/Note.js
@@ -1,22 +1,34 @@
 const mongoose = require("mongoose");
 
 const options = {
-	timestamp: true,
-	strict: false
+    timestamp: true,
+    strict: false
 };
 
 const Note = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true
-		},
-		content: {
-			type: String,
-			required: true
-		}
-	},
-	options
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        isComplete: {
+            type: Boolean,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        },
+        archive: {
+            type: Boolean,
+            required: true
+        }
+    },
+    options
 );
 
 module.exports = mongoose.model("Note", Note);
