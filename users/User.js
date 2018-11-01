bed085dbf136f27a1fb4d936a2d8af7551003af4
JustinSHong <jhong2009@gmail.com>
2018-11-01 15:59:06 -0700

    added new data fields to User model

--- users/User.js
+++ users/User.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
 
 const options = {
-	timestamp: true,
-	strict: false
+    timestamp: true,
+    strict: false
 };
 
 const userSchema = new mongoose.Schema(
-	{
-		username: {
-			type: String,
-			required: true
-		}
-	},
-	options
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        },
+        auth: {
+            type: String,
+            enum: ["email", "google", "twitter", "facebook"],
+            default: "email"
+        },
+        notes: [{ type: ObjectId, ref: "Note", autopopulate: true }]
+    },
+    options
 );
 
 module.exports = mongoose.model("User", userSchema);
